4cc438514cbffe68f1763203820db1223df01d3e
aalbuja2 <andrea.albuja93@gmail.com>
2022-06-19 23:08:24 -0500

    AAL-New Stages

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -15,10 +15,28 @@ pipeline {
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
+        stage('Api Test') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+        stage('E2E test') {
+            steps {
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
     }
     post {
         always {
             junit 'results/*_result.xml'
         }
+        success {  
+             echo 'This will run only if successful'  
+        } 
+        failure {  
+             mail bcc: '', body: "<b>Example</b><br>Project: ${env.JOB_NAME} <br>Build Number: ${env.BUILD_NUMBER} <br> URL de build: ${env.BUILD_URL}", cc: '', charset: 'UTF-8', from: '', mimeType: 'text/html', replyTo: '', subject: "ERROR CI: Project name -> ${env.JOB_NAME}", to: "andrea.albuja93@gmail.com";  
+        } 
     }
 }
